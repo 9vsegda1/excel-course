9a03d53ba6ebc7d86e1e9df3b5dd0b1c2d1e8683
9vsegda1 <makki.nem@yandex.ru>
2020-06-06 12:07:01 +0700

    finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,17 +3,38 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+
+    this.emitter = options.emitter
+    this.unsubs = []
+
+    this.prepare()
+  }
+  // Настраиваем компонент до инит
+  prepare() {
+
+  }
+  // фаер слушателей
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Подписка на события event
+  $on(event, fn) {
+    const unsub = this.emitter.subcribe(event, fn)
+    this.unsubs.push(unsub)
   }
   // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
-
+  // Инициализация компонента
+  // добавление домслушателей
   init() {
     this.initDOMListeners()
   }
-
+  // удаление компонента слушателей
   destroy() {
     this.removeDOMListeners()
+    this.unsubs.forEach(unsub => unsub())
   }
 }
