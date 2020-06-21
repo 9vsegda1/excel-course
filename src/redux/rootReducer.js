3f64929be244efba3a6d293f0aea1728c8ce32dd
9vsegda1 <makki.nem@yandex.ru>
2020-06-21 21:39:34 +0700

    Routing implemented

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,5 @@
 import {TABLE_RESIZE, CHANGE_TEXT, CHANGE_STYLES,
-  APPLY_STYLE, CHANGE_TITLE} from './types'
+  APPLY_STYLE, CHANGE_TITLE, UPDATE_DATE} from './types'
 // pure function
 export function rootReducer(state, action) {
   let field
@@ -36,6 +36,9 @@ export function rootReducer(state, action) {
       }
     case CHANGE_TITLE:
       return {...state, title: action.data}
+
+    case UPDATE_DATE:
+      return {...state, openedDate: new Date().toJSON()}
     default: return state
   }
   // return state
